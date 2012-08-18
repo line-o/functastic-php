8ff008571dd9f6c32d11c0dea74735bb4547cbd9
Juri Leino <juri.leino@spot-media.de>
2012-08-18 15:09:45 +0200

    added y-combinator example

---
+++ examples/yc.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Working Y-combinator in PHP!
+ */
+function Y($f) {
+    return call_user_func(
+        function ($x) use (&$f) {
+            return $f(function ($v) use (&$x) { 
+                return call_user_func(
+                    call_user_func($x, $x), $v); 
+            }); 
+        },
+        function ($x) use (&$f) {
+                return $f(function ($v) use (&$x) {
+                    return call_user_func(
+                        call_user_func($x, $x), $v); 
+            });
+        }
+    );
+}
+
+$factorial = Y(function ($fac) {
+    return function ($n) use (&$fac) {
+        if ($n == 0) { return 1; }
+        else { return $n * $fac($n - 1); }
+    };
+});
+ 
+echo assert(120 === $factorial(5));
